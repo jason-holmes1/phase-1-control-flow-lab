9d87a3f15963b5c483531cf835fc5799bb6b99e4
Jason <holmesjason481@gmail.com>
2024-01-29 16:03:07 -0500

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet >= 400 && feet <= 2000) {
+    return "That will be twenty bucks.";
+  }
+  if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  if (feet > 2500) {
+    return "No can do."
+  }
+  else {
+    return "This one is on me!"
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let truth = (city === "NYC");
+  let mess = null;
+  truth ? mess = "Ok, sounds good." : mess = "No go.";
+  return mess;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
 }
